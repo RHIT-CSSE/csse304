5337e99142b4be979af118102bc722f515379394
rhit-hurtigna <hurtigna@rose-hulman.edu>
2021-11-22 18:13:19 -0500

    Change weights for A03

--- Homework/Assignment_03/A03-testcode.rkt
+++ Homework/Assignment_03/A03-testcode.rkt
@@ -19,16 +19,16 @@
 
 (define test (make-test
   (intersection set-equals?
-    [(intersection '(a b d e f h i j) '(h q r i z)) '(i h) 1]
-    [(intersection '(g h i) '(j k l)) '() 1]
+    [(intersection '(a b d e f h i j) '(h q r i z)) '(i h) 5]
+    [(intersection '(g h i) '(j k l)) '() 3]
     [(intersection '(a p t) '()) '() 1]
     [(intersection '() '(g e t)) '() 1]
   )
 
   (subset? equal?
-    [(subset? '(c b) '(a d b e c)) #t 1]
-    [(subset? '(c b) '(a d b e)) #f 1]
-    [(subset? '(c b) '()) #f 1]
+    [(subset? '(c b) '(a d b e c)) #t 2]
+    [(subset? '(c b) '(a d b e)) #f 2]
+    [(subset? '(c b) '()) #f 2]
     [(subset? '(c b) '(b c)) #t 1]
     [(subset? '(1 3 4) '(1 2 3 4 5)) #t 1]
     [(subset? '() '()) #t 1]
@@ -37,30 +37,30 @@
 
   (relation? equal?
     [(relation? 5) #f 1]
-    [(relation? '()) #t 1]
-    [(relation? '((a b) (b c))) #t 1]
-    [(relation? '((a b) (b a) (a a) (b b))) #t 1]
-    [(relation? '((a b) (b c d))) #f 1]
-    [(relation? '((a b) (c d) (a b))) #f 1]
+    [(relation? '()) #t 2]
+    [(relation? '((a b) (b c))) #t 3]
+    [(relation? '((a b) (b a) (a a) (b b))) #t 2]
+    [(relation? '((a b) (b c d))) #f 3]
+    [(relation? '((a b) (c d) (a b))) #f 2]
     [(relation? '((a b) (c d) "5")) #f 1]
     [(relation? '((a b) . (b c))) #f 1]
   )
 
   (domain set-equals?
-    [(domain '((1 2) (3 4) (1 3) (2 7) (1 6))) '(2 3 1) 1]
-    [(domain '()) '() 1]
+    [(domain '((1 2) (3 4) (1 3) (2 7) (1 6))) '(2 3 1) 6]
+    [(domain '()) '() 4]
   )
 
   (reflexive? eq?
-    [(reflexive? '((a a) (b b) (c d) (b c) (c c) (e e) (c a) (d d))) #t 1]
-    [(not (reflexive? '((a a) (b b) (c d) (b c) (e e) (c a) (d d)))) #t 1]
-    [(not (reflexive? '((a a) (c d) (b c) (c c) (e e) (c a) (d d)))) #t 1]
+    [(reflexive? '((a a) (b b) (c d) (b c) (c c) (e e) (c a) (d d))) #t 3]
+    [(not (reflexive? '((a a) (b b) (c d) (b c) (e e) (c a) (d d)))) #t 3]
+    [(not (reflexive? '((a a) (c d) (b c) (c c) (e e) (c a) (d d)))) #t 4]
     [(reflexive? '()) #t 1]
-    [(not (reflexive? '((c c) (b b) (c d) (b c) (e e) (c a) (d d)))) #t 1]
+    [(not (reflexive? '((c c) (b b) (c d) (b c) (e e) (c a) (d d)))) #t 4]
   )
 
   (multi-set? equal?
-    [(and (multi-set? '()) (not (multi-set? '(a b)))) #t 1]
+    [(and (multi-set? '()) (not (multi-set? '(a b)))) #t 2]
     [(and (multi-set? '((a 2))) (not (multi-set? '((a -1))))) #t 1]
     [(and (multi-set? '((a 2)(b 3))) (not (multi-set? '((a 2) (a 3))))) #t 1]
     [(and (not (multi-set? '(a b))) (not (multi-set? '((a 3) b)))) #t 1]
@@ -74,8 +74,8 @@
 
   (ms-size equal?
     [(ms-size '()) 0 1]
-    [(ms-size '((a 2))) 2 1]
-    [(ms-size '((a 2)(b 3))) 5 1]
+    [(ms-size '((a 2))) 2 2]
+    [(ms-size '((a 2)(b 3))) 5 3]
   )
 
   (last equal?
@@ -85,8 +85,8 @@
   )
 
   (all-but-last equal?
-    [(all-but-last '(1 5 2 4)) '(1 5 2) 1]
-    [(all-but-last '(c)) '() 1]
+    [(all-but-last '(1 5 2 4)) '(1 5 2) 2]
+    [(all-but-last '(c)) '() 2]
     [(all-but-last '(() (()) (()()))) '(() (())) 1]
   )
 ))
