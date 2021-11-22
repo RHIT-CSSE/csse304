95b604ec1bec09e4658be4a8d98e2ae1ba3d0390
rhit-hurtigna <hurtigna@rose-hulman.edu>
2021-11-22 17:50:43 -0500

    Assignment 4

---
+++ Homework/Assignment_04/A04.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(provide matrix-ref matrix? matrix-transpose filter-in invert pascal-triangle)
+
+(define matrix-ref
+  (lambda (a b c)
+    'nyi))
+
+(define matrix?
+  (lambda (a)
+    'nyi))
+
+(define matrix-transpose
+  (lambda (a)
+    'nyi))
+
+(define filter-in
+  (lambda (a b)
+    'nyi))
+
+(define invert
+  (lambda (a)
+    'nyi))
+
+(define pascal-triangle
+  (lambda (a)
+    'nyi))
