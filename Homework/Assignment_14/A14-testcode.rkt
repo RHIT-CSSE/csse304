f4551cfd7868b237bcba8460802b498fe1c3f8c6
rhit-hurtigna <hurtigna@rose-hulman.edu>
2022-01-12 11:23:55 -0500

    A14 test code

---
+++ Homework/Assignment_14/A14-testcode.rkt
@@ -0,0 +1,66 @@
+#lang racket
+
+; To use these tests:
+; Click "Run" in the upper right
+; (r)
+
+; If you find errors in your code, fix them, save your file, click the "Run" button again, and type (r)
+; You can run a specific group of tests using (run-tests group-name)
+
+(require "../testcode-base.rkt")
+(require "interpreter.rkt")
+(provide get-weights get-names individual-test test)
+
+(define test (make-test ; (r)
+  (primitive-procedures equal? ; (run-test primitive-procedures)
+    [(eval-one-exp ' (list (procedure? +) (not (procedure? (+ 3 4))))) '(#t #t) 2] ; (run-test primitive-procedures 1)
+    [(eval-one-exp ' (list (procedure? procedure?) (procedure? (lambda(x) x)) (not (procedure? '(lambda (x) x))))) '(#t #t #t) 2] ; (run-test primitive-procedures 2)
+    [(eval-one-exp ' (list (procedure? list) (procedure? map) (procedure? apply) (procedure? #t))) '(#t #t #t #f) 2] ; (run-test primitive-procedures 3)
+    [(eval-one-exp ' (map procedure? (list map car 3 (lambda(x) x) (lambda x x) ((lambda () 2))))) '(#t #t #f #t #t #f) 2] ; (run-test primitive-procedures 4)
+    [(eval-one-exp '(apply list (list 3 4 5))) '(3 4 5) 2] ; (run-test primitive-procedures 5)
+    [(eval-one-exp ' (list (vector? (vector 3)) (vector-ref (vector 2 4 5) (vector-ref (vector 2 4 5) 0)))) '(#t 5) 1] ; (run-test primitive-procedures 6)
+    [(eval-one-exp '(length '(a b c d e))) 5 1] ; (run-test primitive-procedures 7)
+    [(eval-one-exp '(vector->list '#(a b c))) '(a b c) 1] ; (run-test primitive-procedures 8)
+    [(eval-one-exp ' (list (procedure? list) (procedure? (lambda (x y) (list (+ x y)))) (procedure? 'list))) '(#t #t #f) 3] ; (run-test primitive-procedures 9)
+    [(eval-one-exp '(apply + '(1 2 3 4))) 10 2] ; (run-test primitive-procedures 10)
+  )
+
+  (lambda-regression-tests equal? ; (run-test lambda-regression-tests)
+    [(eval-one-exp '((lambda (x) (+ 1 x)) 5)) 6 1] ; (run-test lambda-regression-tests 1)
+    [(eval-one-exp '((lambda (x) (+ 1 x) (+ 2 (* 2 x))) 5)) 12 1] ; (run-test lambda-regression-tests 2)
+    [(eval-one-exp ' ((lambda (a b) (let ((a (+ a b)) (b (- a b))) (let ((f (lambda (a) (+ a b)))) (f (+ 3 a b))))) 56 17)) 154 3] ; (run-test lambda-regression-tests 3)
+    [(eval-one-exp ' (((lambda (f) ((lambda (x) (f (lambda (y) ((x x) y)))) (lambda (x) (f (lambda (y) ((x x) y)))))) (lambda (g) (lambda (n) (if (zero? n) 1 (* n (g (- n 1))))))) 6)) 720 4] ; (run-test lambda-regression-tests 4)
+    [(eval-one-exp ' (let ((Y (lambda (f) ((lambda (x) (f (lambda (y) ((x x) y)))) (lambda (x) (f (lambda (y) ((x x) y))))))) (H (lambda (g) (lambda (x) (if (null? x) '() (cons (procedure? (car x)) (g (cdr x)))))))) ((Y H) (list list (lambda (x) x) 'list)))) '(#t #t #f) 4] ; (run-test lambda-regression-tests 5)
+  )
+
+  (lambda-with-variable-args equal? ; (run-test lambda-with-variable-args)
+    [(eval-one-exp '((lambda x (car x) (cdr x)) 'a 'b 'c)) '(b c) 5] ; (run-test lambda-with-variable-args 1)
+    [(eval-one-exp '((lambda (x y . z) (cons (+ x y) (cdr z))) 5 4 3 2 1)) '(9 2 1) 4] ; (run-test lambda-with-variable-args 2)
+    [(eval-one-exp ' ((lambda (x y . z) (if (> x y) (car z) (cdr z)) (cadr z)) 5 4 'three 'two 'one)) 'two 4] ; (run-test lambda-with-variable-args 3)
+  )
+
+  (syntactic-expansion equal? ; (run-test syntactic-expansion)
+    [(eval-one-exp '(cond ((< 4 3) 8) ((< 2 3) 7) (else 8))) 7 2] ; (run-test syntactic-expansion 1)
+    [(eval-one-exp '(cond ((< 4 3) 8) ((> 2 3) 7) (else 6))) 6 2] ; (run-test syntactic-expansion 2)
+    [(eval-one-exp '(cond (else 8))) 8 1] ; (run-test syntactic-expansion 3)
+    [(eval-one-exp '(let ((a (vector 3))) (cond ((= (vector-ref a 0) 4) 5) ((begin (vector-set! a 0 (+ 1 (vector-ref a 0))) (= (vector-ref a 0) 4)) 6) (else 10)))) 6 3] ; (run-test syntactic-expansion 4)
+    [(eval-one-exp '(or #f #f 3 #f)) 3 3] ; (run-test syntactic-expansion 5)
+    [(eval-one-exp '(or #f #f #f)) #f 1] ; (run-test syntactic-expansion 6)
+    [(eval-one-exp '(or)) #f 1] ; (run-test syntactic-expansion 7)
+    [(eval-one-exp '(let ((a (vector 5))) (if #t (begin (vector-set! a 0 3) (vector-set! a 0 (+ 3 (vector-ref a 0))) a)))) '#(6) 4] ; (run-test syntactic-expansion 8)
+    [(eval-one-exp '(let ((a (vector 3))) (while (< (vector-ref a 0) 100000) (vector-set! a 0 (* (vector-ref a 0) (vector-ref a 0))) (vector-set! a 0 (quotient (vector-ref a 0) 2))) a)) '#(131072) 3] ; (run-test syntactic-expansion 9)
+    [(eval-one-exp '(let ((a (vector 3))) (while (< (vector-ref a 0) 3) (vector-set! a 0 (* (vector-ref a 0) (vector-ref a 0))) (vector-set! a 0 (quotient (vector-ref a 0) 2))) a)) '#(3) 3] ; (run-test syntactic-expansion 10)
+    [(eval-one-exp '(let ((f (lambda (x) (+ 2 (* 3 x))))) (f (let ((f (lambda (x) (f (* 5 x))))) (f 4))))) 188 8] ; (run-test syntactic-expansion 11)
+    [(eq? (void) (eval-one-exp '(cond ((< 3 3) "this is false") ((< 2 2) "this is false" )))) #t 3] ; (run-test syntactic-expansion 12)
+    [(eval-one-exp '(let* ((x 1) (y (+ x 1))) (if (and (= x 1) (= y 2)) 'correct 'incorrect))) 'correct 3] ; (run-test syntactic-expansion 13)
+    [(eval-one-exp ' (let ((a (vector 4))) (or (begin (vector-set! a 0 (+ 2 (vector-ref a 0))) #f) (begin (vector-set! a 0 (+ 7 (vector-ref a 0))) #t)) a)) '#(13) 3] ; (run-test syntactic-expansion 14)
+    [(eval-one-exp ' (let ((a (vector 5))) (let ((b (begin (vector-set! a 0 7) (list->vector (cons 4 (vector->list a))))) (a 12)) (list->vector (cons a (vector->list b)))))) '#(12 4 7) 5] ; (run-test syntactic-expansion 15)
+    [(eval-one-exp '(let ((a (vector (let ((a 4)) (cond ((negative? 3) (+ a 1)) (else (or #f (+ a 1))))))) (b (vector 0))) (while (positive? (vector-ref a 0)) (vector-set! b 0 (and #t (+ (vector-ref b 0) (vector-ref a 0)))) (vector-set! a 0 (sub1 (vector-ref a 0)))) b)) '#(15) 6] ; (run-test syntactic-expansion 16)
+  )
+
+  (one-armed-if equal? ; (run-test one-armed-if)
+    [(eval-one-exp '(let ((x (vector 7))) (if (< 4 5) (vector-set! x 0 (+ 3 (vector-ref x 0)))) (if (< 4 2) (vector-set! x 0 (+ 6 (vector-ref x 0)))) (vector-ref x 0))) 10 5] ; (run-test one-armed-if 1)
+  )
+))
+
+(implicit-run test) ; run tests as soon as this file is loaded
