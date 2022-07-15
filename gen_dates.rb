43d617f35832892650e2f43a11901ac6f0a41407
Mike Hewner <hewner@rose-hulman.edu>
2022-07-15 15:57:17 -0400

    working on new dates

--- gen_dates.rb
+++ gen_dates.rb
@@ -1,11 +1,13 @@
 require 'date'
 require 'yaml'
 
-start_date = Date.new(2021, 11, 29)
-end_date = Date.new(2022, 2, 20)
-vacation_start = Date.new(2021, 12, 18) #inclusive
-vacation_end = Date.new(2022, 1, 2) # inclusive
+start_date = Date.new(2022, 9, 1)
+end_date = Date.new(2022, 11, 13)
+vacation_start = Date.new(2022, 10, 13) #inclusive
+vacation_end = Date.new(2022, 10, 14) # inclusive
 allowed_days_of_week = [1,2,4,5] # monday is 1
+
+generate_yaml = false #we either generate yaml or csv
 
 day_list = []
 current = start_date;
@@ -17,11 +19,19 @@ while(current <= end_date)
 end
 day_list =  day_list.keep_if { |d| allowed_days_of_week.include?(d.cwday()) }
 class_index = 0;
-string_list = day_list.map { |d|
-  class_index = class_index + 1
-  { "type" => "class_session",
-    "class_num" => class_index,
-    "date" => d.to_time().strftime("%Y-%m-%d 08:00") }
-}
-puts string_list.to_yaml
+
+if generate_yaml then
+  string_list = day_list.map { |d|
+    class_index = class_index + 1
+    { "type" => "class_session",
+      "class_num" => class_index,
+      "date" => d.to_time().strftime("%Y-%m-%d 08:00") }
+  }
+  puts string_list.to_yaml
+else
+  day_list.each { |d|
+    class_index = class_index + 1
+    puts " Day #{class_index} , #{d.to_time().strftime("%Y-%m-%d 23:59")}"
+  }
+end
 $stderr.puts "#{day_list.length} days output"
