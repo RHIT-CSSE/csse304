ff0e779d3bc7a6c63c744d081504fe3c96133238
Michael Hewner <hewner@gmail.com>
2023-09-07 09:53:40 -0400

    fixing example

--- ClassMaterials/MapApply/named-let-example.rkt
+++ ClassMaterials/MapApply/named-let-example.rkt
@@ -4,8 +4,10 @@
 
 (define has-1-even?
   (lambda (lst)
-    (= 1 (let recurse ((lon lst) (count 0))
-           (cond [(null? lon) count]
-                 [(not (zero? (car lon))) (recurse (cdr lon) count)]
-                 [(even? count) (recurse (cdr lon) 1)]
-                 [else 2])))))
+    (= 1 (let recurse ((lst lst) (count 0))
+           (cond [(null? lst) count]
+                 [(even? (car lst))
+                  (if (zero? count)
+                      (recurse (cdr lst) (add1 count))
+                      2)]
+                 [else (recurse (cdr lst) count)])))))
