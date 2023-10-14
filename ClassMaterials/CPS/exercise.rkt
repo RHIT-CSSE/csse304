7c013f8ad4c1022b16d173c25775d01f189be9fb
Michael Hewner <hewner@gmail.com>
2023-10-13 23:18:34 -0400

    CPS conversion starter code

--- ClassMaterials/CPS/exercise.rkt
+++ ClassMaterials/CPS/exercise.rkt
@@ -13,29 +13,20 @@
 
 (define-datatype continuation continuation?
   [init-k]
-  [flatten-cdr-k
-   (ls list?)
-   (k continuation?)]
-  [flatten-car-k
-   (flattened-cdr list?)
-   (k continuation?)]   
+  ; more types here 
   )
 
 (define flatten-cps
   (lambda (ls k)
-    (if (null? ls)
-	(apply-k k ls)
-	(flatten-cps (cdr ls) (flatten-cdr-k ls k)))))
+    'nyi
+    ))
 
 
 (define apply-k
   (lambda (k v)
 	(cases continuation k
           [init-k () v]
-          [flatten-cdr-k (ls k)
-                         (if (list? (car ls))
-                             (flatten-cps (car ls) (flatten-car-k v k))
-                             (apply-k k (cons (car ls) v)))]
-          [flatten-car-k (flattened-cdr k)
-                         (apply-k k (append v flattened-cdr))]
+          ; more code here
           )))
+
+(flatten-cps '(a (b c) (d ((e (f))) g)) '(init-k))
