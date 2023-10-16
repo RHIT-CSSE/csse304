5651dfd729272137e4a22117164e6e34e97de2ec
Michael Hewner <hewner@gmail.com>
2023-10-16 19:53:21 -0400

    moving exercises around

---
+++ ClassMaterials/CPSFinal/exercise-solution.rkt
@@ -0,0 +1,77 @@
+#lang racket
+(require "chez-init.rkt")
+
+;; Below I've provided my solution to combine-conseq.  Most of the work
+;; is actually done in a helper function - combine-consec-helper.  Convert
+;; both functions to CPS.
+
+;; To remind you, here's the description of combine-conseq
+
+;; ;; combine-consec takes a *sorted* list of integers and combines them
+;; ;; into a series of ranges.  It compresses sequences of consecutive
+;; ;; numbers into ranges - so for example the list (1 2 3 4) becomes ((1
+;; ;; 4)) representing the single range 1-4.  However, when numbers are
+;; ;; missing then there must be multiple ranges - e.g. (1 2 3 6 7 8)
+;; ;; becomes ((1 3) (6 8)) representing 1-3,6-8.  If a number is by
+;; ;; itself (i.e. it is not consecutive with either its successor or
+;; ;; predecessor) it can be in a range by itself so (1 2 4 7) becomes
+;; ;; ((1 2) (4 4) (7 7)).
+
+
+(define (combine-consec-orig lst)
+  (if (null? lst)
+      '()
+      (combine-consec-helper-orig
+       (car lst)
+       (car lst)
+       (cdr lst))))
+
+
+(define (combine-consec-helper-orig first last lst)
+  (cond [(null? lst) (list (list first last))]
+        [(= (add1 last) (car lst))
+         (combine-consec-helper-orig first (add1 last) (cdr lst))]
+        [else (cons (list first last) (combine-consec-helper-orig (car lst) (car lst) (cdr lst)))]))
+
+
+
+
+(define-datatype continuation continuation?
+  [init-k]
+  [step1 (first number?) (last number?) (k continuation?)]
+  ; more types here 
+  )
+
+(define apply-k
+  (lambda (k v)
+	(cases continuation k
+          [init-k () v]
+          [step1 (first last k)
+                 (apply-k k (cons (list first last) v))]
+          )))
+          ; more code here
+          
+
+(define (combine-consec lst k)
+  (if (null? lst)
+      (apply-k k '())
+      (combine-consec-helper-cps
+       (car lst)
+       (car lst)
+       (cdr lst)
+       k)))
+
+(define (combine-consec-helper-cps first last lst k)
+  (cond [(null? lst) (apply-k k (list (list first last)))]
+        [(= (add1 last) (car lst))
+         (combine-consec-helper-cps first (add1 last) (cdr lst) k)]
+        [else (combine-consec-helper-cps
+               (car lst)
+               (car lst)
+               (cdr lst)
+               (step1 first last k))]))
+
+
+
+(combine-consec-orig '(1 2 3 6 7 8 13))
+(combine-consec '(1 2 3 6 7 8 13) (init-k))
--- ClassMaterials/CPSMore2/exercise.rkt
+++ ClassMaterials/CPSMore2/exercise.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "chez-init.rkt")
+(require racket/trace)
 
 (define flatten
   (lambda (lst)
@@ -13,20 +14,30 @@
 
 (define-datatype continuation continuation?
   [init-k]
+  [step1 (lst list?) (k continuation?)]
+  [step2 (old-v list?) (k continuation?)]
   ; more types here 
   )
 
 (define flatten-cps
-  (lambda (ls k)
-    'nyi
-    ))
+  (lambda (lst k)
+    (if (null? lst)
+        (apply-k k '())
+        (flatten-cps (cdr lst) (step1 lst k)))))
 
 
 (define apply-k
   (lambda (k v)
 	(cases continuation k
           [init-k () v]
-          ; more code here
+          [step1 (lst k)
+                 (if (list? (car lst))
+                     (flatten-cps (car lst) (step2 v k))
+                     (apply-k k (cons (car lst) v)))]
+          [step2 (old-v k)
+                 (apply-k k (append v old-v))]
           )))
+
+(trace flatten-cps apply-k)
 
 (flatten-cps '(a (b c) (d ((e (f))) g)) '(init-k))
