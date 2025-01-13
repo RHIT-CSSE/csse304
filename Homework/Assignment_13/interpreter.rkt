40065c335b56ccef469559a590f11d3f97d976c8
Michael Wollowski <wollowski@rose-hulman.edu>
2025-01-13 13:12:33 -0500

    Update interpreter.rkt
    Much simplified. Students should implement something too.

--- Homework/Assignment_13/interpreter.rkt
+++ Homework/Assignment_13/interpreter.rkt
@@ -14,16 +14,7 @@
 
 (define-datatype expression expression?  
   [var-exp        ; variable references
-   (id symbol?)]
-  [lit-exp        ; "Normal" data.  Did I leave out any types?
-   (datum
-    (lambda (x)
-      (ormap 
-       (lambda (pred) (pred x))
-       (list number? vector? boolean? symbol? string? pair? null?))))]
-  [app-exp        ; applications
-   (rator expression?)
-   (rands (list-of? expression?))]  
+   (id symbol?)] 
   )
 	
 
@@ -34,18 +25,12 @@
   
 (define-datatype environment environment?
   [empty-env-record]
-  [extended-env-record
-   (syms (list-of? symbol?))
-   (vals (list-of? scheme-value?))
-   (env environment?)])
+)
 
 
 ; datatype for procedures.  At first there is only one
 ; kind of procedure, but more kinds will be added later.
 
-(define-datatype proc-val proc-val?
-  [prim-proc
-   (name symbol?)])
 
   
 ;-------------------+
@@ -70,10 +55,6 @@
     (cond
       [(symbol? datum) (var-exp datum)]
       [(number? datum) (lit-exp datum)]
-      [(pair? datum)
-       (cond
-         [else (app-exp (parse-exp (1st datum))
-                        (map parse-exp (cdr datum)))])]
       [else (error 'parse-exp "bad expression: ~s" datum)])))
 
 
@@ -91,28 +72,6 @@
   (lambda ()
     (empty-env-record)))
 
-(define extend-env
-  (lambda (syms vals env)
-    (extended-env-record syms vals env)))
-
-(define list-find-position
-  (lambda (sym los)
-    (let loop ([los los] [pos 0])
-      (cond [(null? los) #f]
-            [(eq? sym (car los)) pos]
-            [else (loop (cdr los) (add1 pos))]))))
-	    
-(define apply-env
-  (lambda (env sym) 
-    (cases environment env 
-      [empty-env-record ()      
-                        (error 'env "variable ~s not found." sym)]
-      [extended-env-record (syms vals env)
-                           (let ((pos (list-find-position sym syms)))
-                             (if (number? pos)
-                                 (list-ref vals pos)
-                                 (apply-env env sym)))])))
-
 
 ;-----------------------+
 ;                       |
@@ -120,7 +79,7 @@
 ;                       |
 ;-----------------------+
 
-; To be added in assignment 14.
+; To be added later.
 
 ;---------------------------------------+
 ;                                       |
@@ -128,7 +87,7 @@
 ;                                       |
 ;---------------------------------------+
 
-; To be added in assignment 18a.
+; To be added later
 
 
 ;-------------------+
@@ -137,80 +96,8 @@
 ;                   |
 ;-------------------+
 
-; top-level-eval evaluates a form in the global environment
 
-(define top-level-eval
-  (lambda (form)
-    ; later we may add things that are not expressions.
-    (eval-exp form)))
 
-; eval-exp is the main component of the interpreter
 
-(define eval-exp
-  (lambda (exp)
-    (cases expression exp
-      [lit-exp (datum) datum]
-      [var-exp (id)
-               (apply-env init-env id)]
-      [app-exp (rator rands)
-               (let ([proc-value (eval-exp rator)]
-                     [args (eval-rands rands)])
-                 (apply-proc proc-value args))]
-      [else (error 'eval-exp "Bad abstract syntax: ~a" exp)])))
 
-; evaluate the list of operands, putting results into a list
 
-(define eval-rands
-  (lambda (rands)
-    (map eval-exp rands)))
-
-;  Apply a procedure to its arguments.
-;  At this point, we only have primitive procedures.  
-;  User-defined procedures will be added later.
-
-(define apply-proc
-  (lambda (proc-value args)
-    (cases proc-val proc-value
-      [prim-proc (op) (apply-prim-proc op args)]
-      ; You will add other cases
-      [else (error 'apply-proc
-                   "Attempt to apply bad procedure: ~s" 
-                   proc-value)])))
-
-(define *prim-proc-names* '(+ - * add1 sub1 cons =))
-
-(define init-env         ; for now, our initial global environment only contains 
-  (extend-env            ; procedure names.  Recall that an environment associates
-   *prim-proc-names*   ;  a value (not an expression) with an identifier.
-   (map prim-proc      
-        *prim-proc-names*)
-   (empty-env)))
-
-; Usually an interpreter must define each 
-; built-in procedure individually.  We are "cheating" a little bit.
-
-(define apply-prim-proc
-  (lambda (prim-proc args)
-    (case prim-proc
-      [(+) (+ (1st args) (2nd args))]
-      [(-) (- (1st args) (2nd args))]
-      [(*) (* (1st args) (2nd args))]
-      [(add1) (+ (1st args) 1)]
-      [(sub1) (- (1st args) 1)]
-      [(cons) (cons (1st args) (2nd args))]
-      [(=) (= (1st args) (2nd args))]
-      [else (error 'apply-prim-proc 
-                   "Bad primitive procedure name: ~s" 
-                   prim-proc)])))
-
-(define rep      ; "read-eval-print" loop.
-  (lambda ()
-    (display "--> ")
-    ;; notice that we don't save changes to the environment...
-    (let ([answer (top-level-eval (parse-exp (read)))])
-      ;; TODO: are there answers that should display differently?
-      (pretty-print answer) (newline)
-      (rep))))  ; tail-recursive, so stack doesn't grow.
-
-(define eval-one-exp
-  (lambda (x) (top-level-eval (parse-exp x))))
