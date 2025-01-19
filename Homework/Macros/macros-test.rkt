d38ba0d565ec6d6c132f728c9b0b85434622dfaf
Michael Wollowski <wollowski@rose-hulman.edu>
2025-01-19 17:02:43 -0500

    Update macros-test.rkt
    Removed test cases for problems removed from the assignment. Need to add test cases for new problems.

--- Homework/Macros/macros-test.rkt
+++ Homework/Macros/macros-test.rkt
@@ -13,20 +13,7 @@
 
 (define test (make-test ; (r)
 
-  (my-let equal? ; (run-test my-let)
-    [(my-let foo ((a 1)) a) 1 1] ; (run-test my-let 1)
-    [(my-let foo ((bar 'sym) (baz 'sym)) (and (procedure? foo) (symbol? bar) (symbol? baz))) #t 1]
-    [(my-let loop ((L (quote (1 2 3 4 5 6 7 8 9 10))) (A 0)) (if (null? L) A (loop (cdr L) (+ (car L) A)))) 55 1] ; (run-test my-let 2)
-    [(my-let ((a 5)) (+ 3 (my-let fact ((n a)) (if (zero? n) 1 (* n (fact (- n 1))))))) 123 4] ; (run-test my-let 3)
-    [((lambda (n) (my-let fact2 ((acc 1) (n n)) (if (zero? n) acc (fact2 (* n acc) (sub1 n))))) 4) 24 1]
-    )
-
-  (null-let equal?
-            [(null-let (a b) (list a b)) '(() ()) 2]
-            [(null-let (a b c) (set! a 7) (set! b 12) (list a b c)) '(7 12 ()) 2]
-            [(null-let () 'foobar) 'foobar 2]
-            [(null-let (z) (list z (let [(z 3)] z))) '(() 3) 2]
-            [(let ((x 3) (y 4)) (null-let (x) (list x y))) '(() 4) 2])
+;; init
   
  (all-equal equal?
              [(all-equal 1 1 1 1) #t 1]
@@ -139,28 +126,8 @@
                       (< 4 'large)
                       (else 'huge)))) 'large 6]
     )
-            
 
-
-  (ifelse equal?
-          [(list (ifelse #t 1 else 2) (ifelse #f 1 else 2)) '(1 2) 6]
-          [(let ((v #t)) (list (ifelse v v else 2) (ifelse (not v) 1 else 2))) '(#t 2) 6]
-          [(let ((v #t)) (list (ifelse v (set! v 1) (set! v (add1 v)) v else 2 6)
-                               (ifelse (not v) 1 2 else 3 4 5 6))) '(2 6) 8]
-)
-    (let-destruct equal? ; (run-test let-destruct)          
-    [(let-destruct a (+ 1 10) (+ 1 a)) 12 0.1] ; (run-test let-destruct 1)
-    [(let-destruct () '() 'youll-want-this-base-case) 'youll-want-this-base-case 0.1]
-    [(let-destruct (a) (list 10) (+ 1 a)) 11 0.1]
-    [(let-destruct (a b) (cons 1 (cons 2 '())) (cons a b)) '(1 . 2) 0.1]
-    [(let-destruct ((a) b) '((1) 2) (cons a b)) '(1 . 2) 0.1]
-    [(let-destruct (a b) '((1) ((2))) (cons a b)) '((1) . ((2))) 0.1]
-    [(let-destruct ((a) ((b)) (((c)))) '((1) ((2)) (((3)))) (list a b c)) '(1 2 3) 0.1]
-    [(let-destruct ((a b) c) '((1 2) 3) (list a b c)) '(1 2 3) 0.1]
-    [(let-destruct ((a b) c) '((1 2) 3) (list a b c) 'body2) 'body2 0.1]
-    [(let ((myval '((1 2) (3 4) (5 (6)))))
-       (let-destruct ((a b) c (d (e))) myval (list a b c d e))) '(1 2 (3 4) 5 6) 0.1]
-  )
+;; for loop
 
               
 
