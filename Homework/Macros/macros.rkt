f8ef2fd36546fcc7e4bee401ee60e6aea5e533e1
Michael Wollowski <wollowski@rose-hulman.edu>
2025-01-20 18:37:45 -0500

    Update macros.rkt
    Updated starer code.

--- Homework/Macros/macros.rkt
+++ Homework/Macros/macros.rkt
@@ -4,42 +4,24 @@
 ; this adds some other useful prodcedures
 (require (for-syntax racket/list)) 
 
-(provide my-let null-let all-equal begin-unless range-cases ifelse let-destruct)
+(provide init all-equal begin-unless range-cases for)
 
-(define-syntax (my-let stx)
-  (syntax-case stx ()
-    [(my-let ((name value) ...) bodies ...)
-     #'((lambda (name ...) bodies ...) value ...)]
-    [(my-let _ ...) ; <- NOTE THIS PATTERN IS NOT RIGHT
-     #''nyi] ; the above pattern is just a way to ensure the test cases run
-             ; and expand into the expression 'nyi rather than crashing
+(define-syntax init
+   (syntax-rules ()
+     ))
+
+(define-syntax all-equal
+  (syntax-rules ()
     ))
 
-(define-syntax (null-let stx)
-   (syntax-case stx ()
-     [(null-let _ ...)
-      #''nyi]))
+(define-syntax begin-unless
+  (syntax-rules ()
+    ))
 
-(define-syntax (all-equal stx)
-  (syntax-case stx ()
-    [(all-equal _ ...)
-     #''nyi]))
+(define-syntax range-cases
+  (syntax-rules (< else)
+    ))   
 
-(define-syntax (begin-unless stx)
-  (syntax-case stx ()
-    [(begin-unless _ ...)
-     #''nyi]))
-
-             
-(define-syntax (range-cases stx)
-  (syntax-case stx (< else)
-    [(range-cases _ ...) #''nyi]))   
-
-
-
-(define-syntax (let-destruct stx)
-  (syntax-case stx ()
-    [(let-destruct _ ...) #''nyi]))
-
-(define-syntax (ifelse stx)
-  #'(error "nyi"))
+(define-syntax for
+  (syntax-rules (:)
+    ))  
